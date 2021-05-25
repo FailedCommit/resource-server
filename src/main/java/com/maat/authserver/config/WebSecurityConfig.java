add6cfa64e161fbdd3cb4b6869895998c53b4c1e
Mohit Kumar <mkumar@tekion.com>
2021-05-25 23:40:42 +0530

    Relaxes security for h2 console

--- src/main/java/com/maat/authserver/config/WebSecurityConfig.java
+++ src/main/java/com/maat/authserver/config/WebSecurityConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -44,10 +44,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf(c -> c.ignoringAntMatchers("/user/**", "/client/**"));
+        http.csrf(c -> c.ignoringAntMatchers("/user/**", "/client/**", "/h2-console/**"));
         http.authorizeRequests()
                 .mvcMatchers("/user/**").permitAll()
-                .mvcMatchers("/client/**").permitAll();
+                .mvcMatchers("/client/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll();
         super.configure(http);
+    }
+
+    /**
+     * Override this method to configure {@link WebSecurity}. For example, if you wish to
+     * ignore certain requests.
+     * <p>
+     * Endpoints specified in this method will be ignored by Spring Security, meaning it
+     * will not protect them from CSRF, XSS, Clickjacking, and so on.
+     * <p>
+     * Instead, if you want to protect endpoints against common vulnerabilities, then see
+     * {@link #configure(HttpSecurity)} and the {@link HttpSecurity#authorizeRequests}
+     * configuration method.
+     *
+     * @param web
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
     }
 }
